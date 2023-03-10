f5401b8176128c5bd48deac5d3a79b0157525e1b
lexborelli <99988113+lexborelli@users.noreply.github.com>
2023-03-10 23:50:01 +0000

    added variable to missed attempts and phrases

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const qwerty = document.getElementById('qwerty');
 const phase = document.getElementById('phrase');
-const resetButton = document.querySelector('.btn_reset');
+const resetButton = document.getElementsByClassName('.btn_reset');
+
+
+let missed = 0; 
+
+//create an array named phrases
+
+const phrase = ['Easier said than done' , 'Lose your marbles' , 'Give it a shot' , 'Never in a million years' , 'keep your chin up' , 'That is the last straw']; 
