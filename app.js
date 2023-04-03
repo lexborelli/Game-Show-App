a6476e48bdc11ea9b98e43776eb470856a6d66ac
lexborelli <99988113+lexborelli@users.noreply.github.com>
2023-04-03 23:08:39 +0000

    correcting code

--- app.js
+++ app.js
@@ -2,19 +2,52 @@
 
     const phrase = document.querySelector("#phrase");
 
-    const btn__reset = document.querySelectorAll(".btn__reset");
+    const resetButton = document.querySelectorAll(".btn__reset");
 
     const overlay = document.querySelector("#overlay"); 
 
-    const letter = document.querySelectorAll(".letter");
 
     let missed = 0; 
 
     //Attach an event listener to the “Start Game” button to hide the start screen overlay
 
-   btn__reset.addEventListener('click', () => {
-        document.getElementById('overlay').style.display = 'none'; 
+   resetButton.addEventListener('click', () => {
+       document.getElementById('overlay').style.display = 'none'; 
 
     });
 
- 
+    // An array of phrases to choose from
+
+    const phrases = [
+        'Easier said than done',
+        'Lose your marbles',
+        'Give it a shot',
+        'Never in a million years',
+        'keep your chin up',
+        'That is the last straw'
+    ]; 
+
+    // Returns a random phrase from the phrases array as an array of characters
+    function getRandomPhraseAsArray (arr) {
+        const randomOne = Math.floor(Math.random() * arr.length);
+        return arr [randomOne].split('');  
+    }
+
+    const phraseArray = getRandomPhraseAsArray(phrases);
+
+    function addPhraseToDisplay (arr) {
+        for (let i = 0; i < arr.length; i++) {
+            const li = document.createElement('li'); 
+            li.textContent = arr[i]; 
+            phrase.appendChild(li); 
+            if (li.textContent !== ' ') {
+                li.className = 'letter';
+            } else {
+                li.className = 'letter';
+            }
+        }
+    }
+
+    addPhraseToDisplay(phraseArray); 
+
+//check for letter 
